402c41770d0e1c876a129c46eaa8779ec6d84d5d
Brendan Whitfield <brendanw@windworksdesign.com>
2014-09-17 20:25:24 -0400

    finally fixed buffer problem. ran out of RAM...

--- arduino/openscope/openscope.ino
+++ arduino/openscope/openscope.ino
@@ -6,8 +6,13 @@
 	#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-#define ZERO 256
+#define SIGNAL_BUFFER_SIZE 512
 
+
+int signalBuffer[SIGNAL_BUFFER_SIZE];
+int current = 0;
+
+int pin = 0;
 
 //which analog pins are being watched
 char pinWatch = 0;
@@ -20,30 +25,44 @@ void setup()
 	cbi(ADCSRA,ADPS1);
 	cbi(ADCSRA,ADPS0);
 
+  for(int k = 0; k < SIGNAL_BUFFER_SIZE; k++)
+  {
+    signalBuffer[k] = 0;
+  }
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+
 	Serial.begin(38400);
 }
 
 void loop()
 {
-	for(int p = 0; p < 6; p++)
-	{
-		if(bitRead(pinWatch, p))
-		{
-			sendPin(p);
-		}
-	}
+  current = current % SIGNAL_BUFFER_SIZE;
+  if(current == 0) { sendBuffer(); }
+  signalBuffer[current] = analogRead(pin);
+  current++;
+  delay(1);
 }
 
-void sendPin(int p)
+void sendBuffer()
+{
+  digitalWrite(13, HIGH);
+  for(int i = 0; i < SIGNAL_BUFFER_SIZE; i++)
+  {
+    sendPin(pin, signalBuffer[i]);
+  }
+  digitalWrite(13, LOW);
+  delay(500);
+}
+
+void sendPin(int p, int val)
 {
 	//0 0 p p p x x x
         //1 x x x x x x x
         
         //0 1 0 t t t t t
         //0 1 1 t t t t t
-
-        int val = analogRead(p);
-        val = analogRead(p);
         
         //make sure that the signal is only 10 bits
         val &= 0b1111111111; //xxxxxxxxxx
@@ -66,6 +85,6 @@ void sendPin(int p)
 //called when a byte arrives from the computer
 void serialEvent()
 {
-	pinWatch = Serial.read();
+	pinWatch = (uint8_t) Serial.read();
 	Serial.write(pinWatch);
 }
